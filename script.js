0a6aae3599f512120ea9b3bda528542542729c12
Auxurya <cyber@auxurya.com>
2022-01-04 00:23:43 -0500

    Created functions: add(), subtract(), multiply(), divide()

---
+++ script.js
@@ -0,0 +1,12 @@
+const add = (x, y) => {
+    return x + y;
+};
+const subtract = (x, y) => {
+    return x - y;
+};
+const multiply = (x, y) => {
+    return x * y;
+};
+const divide = (x, y) => {
+    return x / y;
+};
