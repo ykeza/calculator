abf6b03b0fb3a788b8574d8d08f25f59d5488f2c
Auxurya <cyber@auxurya.com>
2022-01-04 00:55:22 -0500

    Created a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ const multiply = (x, y) => {
 const divide = (x, y) => {
     return x / y;
 };
+const operate = (a, b, op) => {
+    switch (op) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+        default:
+            return 'Invalid operator!';
+            break;
+    }
+};
