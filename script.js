a09b72cef6a168db9cd34083e80eb661a5847009
Auxurya <cyber@auxurya.com>
2022-01-06 11:56:26 -0500

    Got the calculator to work in the console as I click its buttons + added digital font

--- script.js
+++ script.js
@@ -10,22 +10,83 @@ const multiply = (x, y) => {
 const divide = (x, y) => {
     return x / y;
 };
-const operate = (a, b, op) => {
+const negate = num => {
+    return num - num * 2;
+};
+const operate = (x, y, op) => {
     switch (op) {
         case "+":
-            return add(a, b);
+            return add(x, y);
             break;
         case "-":
-            return subtract(a, b);
+            return subtract(x, y);
             break;
         case "*":
-            return multiply(a, b);
+            return multiply(x, y);
             break;
         case "/":
-            return divide(a, b);
+            return divide(x, y);
             break;
         default:
-            return 'Invalid operator!';
+            return 'ERR';
             break;
     }
 };
+let firstOp = '';
+let firstStore = '';
+let oper = '';
+let operStore = '';
+let secondOp = '';
+let secondStore = '';
+let sum = '';
+
+const firstOper = num => {
+    let nums = String(firstOp).replace('.', '').length;
+    if (nums >= 7) {
+        return;
+    } else {
+        let _num = num.target.innerHTML;
+        firstOp += `${_num}`;
+        firstStore = firstOp;
+        console.log(firstStore);
+    };
+};
+const secondOper = num => {
+    let nums = String(secondOp).replace('.', '').length;
+    if (nums >= 7) {
+        return;
+    } else {
+        let _num = num.target.innerHTML;
+        secondOp += `${_num}`;
+        secondStore = secondOp;
+        console.log(secondStore);
+    };
+};
+const numbers = document.querySelectorAll('.number');
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', e => {
+        if (!oper && !secondOp) {
+            firstOper(e);
+        } else {
+            secondOper(e);
+        }
+    });
+};
+const operators = document.querySelectorAll('.operator');
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', e => {
+        if (!secondOp && firstOp) {
+            oper = e.target.innerHTML;
+            console.log(oper);
+        } else if (firstOp && secondOp) {
+            operStore = oper;
+            sum = operate(Number(firstStore), Number(secondStore), operStore);
+            firstStore = sum;
+            oper = e.target.innerHTML;
+            secondOp = '';
+            console.log(sum);
+            console.log(oper);
+        };
+    });
+};
+ 
